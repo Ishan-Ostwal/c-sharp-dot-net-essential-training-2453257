26d268a3734cebd0935d6bb08632dfb3c71f70fa
Ishan-Ostwal <ishan.ostwal@insurity.com>
2026-02-07 01:56:36 +0530

    searching files

--- Start/Strings/Searching/Program.cs
+++ Start/Strings/Searching/Program.cs
@@ -4,14 +4,15 @@
 string teststr = "The quick brown Fox jumps over the lazy Dog";
 
 // TODO: Contains determines whether a string contains certain content
-
+Console.WriteLine(teststr.Contains("quick"));
 
 // TODO: StartsWith and EndsWith determine if a string starts 
 // or ends with a given test string
-
-
+Console.WriteLine(teststr.StartsWith("fox",StringComparison.CurrentCultureIgnoreCase));
+Console.WriteLine(teststr.EndsWith("Dog"));
 // TODO: IndexOf, LastIndexOf finds the index of a substring
-
+Console.WriteLine(teststr.IndexOf("jumps", StringComparison.CurrentCultureIgnoreCase));
+Console.WriteLine(teststr.LastIndexOf("g"));
 
 // TODO: Determining empty, null, or whitespace
 string str1 = null;
